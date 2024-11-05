0ae3f7c72ddb93153d16fb333533a50ca54e0c5b
Vyshnavisree <141749433+Vyshnavisree@users.noreply.github.com>
2024-11-05 11:49:56 -0600

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import { ENV_VARS } from "./envVars.js";
 
+
 export const connectDB = async () => {
 	try {
 		const conn = await mongoose.connect(ENV_VARS.MONGO_URI);
