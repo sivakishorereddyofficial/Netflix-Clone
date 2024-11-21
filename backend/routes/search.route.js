2e3f4be8f2eb8464177a75c3088f65c7fe725fa2
SivaChandraRamaNaiduK <sivachandraramanaidu.vnof52@gmail.com>
2024-11-21 15:34:51 -0600

    added search routes

--- backend/routes/search.route.js
+++ backend/routes/search.route.js
@@ -1,20 +1,22 @@
 import express from "express";
 import {
-	getSearchHistory,
-	removeItemFromSearchHistory,
-	searchMovie,
-	searchPerson,
-	searchTv,
+  getSearchHistory,
+  removeItemFromSearchHistory,
+  searchMovie,
+  searchPerson,
+  searchTv,
 } from "../controllers/search.controller.js";
 
+// Create a new router instance
 const router = express.Router();
 
+// Search routes
 router.get("/person/:query", searchPerson);
 router.get("/movie/:query", searchMovie);
 router.get("/tv/:query", searchTv);
 
+// Search history routes
 router.get("/history", getSearchHistory);
-
 router.delete("/history/:id", removeItemFromSearchHistory);
 
 export default router;
