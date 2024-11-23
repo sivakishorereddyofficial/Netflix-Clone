72efd1461325860769fad2fd65df7c8cbb10eac0
SivaChandraRamaNaiduK <sivachandraramanaidu.vnof52@gmail.com>
2024-11-22 23:37:02 -0600

    user credentials

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,28 +1,28 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({
-	username: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-	image: {
-		type: String,
-		default: "",
-	},
-	searchHistory: {
-		type: Array,
-		default: [],
-	},
+const accountSchema = new mongoose.Schema({
+  userid: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  emailAddress: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  hashedPassword: {
+    type: String,
+    required: true,
+  },
+  profilePicture: {
+    type: String,
+    default: "",
+  },
+  activityLog: {
+    type: Array,
+    default: [],
+  },
 });
 
-export const User = mongoose.model("User", userSchema);
+export const Account = mongoose.model("Account", accountSchema);
