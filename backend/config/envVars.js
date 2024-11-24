0e7786965fc6ddeb4cfe8ac3e47fede03fd59833
SivaChandraRamaNaiduK <sivachandraramanaidu.vnof52@gmail.com>
2024-11-24 01:47:57 -0600

    set jwt secret

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -7,7 +7,7 @@ dotenv.config();
 export const CONFIG = {
   DATABASE_URL: process.env.MONGO_URI,
   APP_PORT: process.env.PORT || 5000,
-  TOKEN_SECRET: process.env.JWT_SECRET,
+  TOKN_SECRET: process.env.JWT_SECRET,
   ENVIRONMENT: process.env.NODE_ENV,
   TMDB_KEY: process.env.TMDB_API_KEY,
 };
